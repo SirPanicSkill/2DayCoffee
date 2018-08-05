dd480ea18411552a62c61c6cc5e53aac97fc362a
SirPanicSkill <txwnik@live.ru>
2018-08-05 16:28:16 +0300

    добавли фаиконку. переключение страниц

--- js/script.js
+++ js/script.js
@@ -1,23 +1,31 @@
+/* JqvaScript code */
+document.addEventListener('DOMContentLoaded', function() {
+	//sideNav init
+	var sideNavElems = document.querySelectorAll('.sidenav');
+	M.Sidenav.init(sideNavElems, {edge: 'right'});
+	//tabs init
+	var tabsElems = document.querySelector('.tabs');
+	M.Tabs.init(tabsElems, {});
+});
+
 /*globals jQuery, document */
 $(function() {
 	"use strict";
-	$(".button-collapse").sideNav();
-	//левое меню для телефонов
-	$('.button-collapse').sideNav({
-		menuWidth: 200, // Default is 300
-		edge: 'right', // Choose the horizontal origin
-		closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
-		draggable: true, // Choose whether you can drag to open on touch screens,
-		onOpen: function(el) { /* Do Stuff*/ }, // A function to be called when sideNav is opened
-		onClose: function(el) { /* Do Stuff*/ }, // A function to be called when sideNav is closed
-	});
-
 	//навигация меню
-	$('.menu-click').each(function() {
+	$('.menu-click').each(function(e) {
 		var menu_count = $(this).attr('data-attr');
-		$(this).on('click', function() {
+		$(this).on('click', function(e) {
+			e.preventDefault();
 			$('section[data-attr='+ menu_count +']').addClass('sec-active').siblings().removeClass('sec-active');
+			$('.tabs').find('.indicator').css('opacity', '1');
 		});
+	});
+	//переход по лого на главную
+	$('.logo-link').on('click', function(e) {
+		e.preventDefault();
+		$('section[data-attr]').removeClass('sec-active').siblings().removeClass('sec-active');
+		$('section.section-main').addClass('sec-active');
+		$('.tabs').find('.indicator').css('opacity', '0');
 	});
 
 	//смена языка
@@ -30,7 +38,7 @@ $(function() {
 			success : function (response) {
 				LANGUAGE = response; //записываем в глобальную переменную, а вдруг пригодиться
 				$('body').find("[lang]").each(function () {//ищем все элементы с атрибутом
-					var lang = LANGUAGE[ $(this).attr('lang') ]; //берем нужное значение по атрибуту lng
+					var lang = LANGUAGE[ $(this).attr('lang') ]; //берем нужное значение по атрибуту lang
 					var tag = $(this)[0].tagName.toLowerCase();
 					switch (tag) {//узнаем название тега
 						case "input":
@@ -43,6 +51,9 @@ $(function() {
 				});
 			}
 		});
+		let tabsElems = document.querySelector('.tabs');
+		setTimeout(M.Tabs.init(tabsElems, {}).updateTabIndicator(), 1000);
+		;
 	};
 	$('.lang-ru').on('click', function(e) {
 		e.preventDefault();
